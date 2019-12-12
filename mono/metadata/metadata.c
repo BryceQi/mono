1180195a71e7f27f19faa32b2efc95629cb7c79b
UnityAlex <alexthibodeau@unity3d.com>
2019-12-12 11:37:12 -0500

    Sourcing a collection of changes from:
    https://github.com/Unity-Technologies/mono/commit/4de558008131f3a2e998433544392132a15b4918
    
    Getting classlibs building by dealing with mostly deprecated function usage.

--- mono/metadata/metadata.c
+++ mono/metadata/metadata.c
@@ -5513,6 +5513,7 @@ mono_metadata_localscope_from_methoddef (MonoImage *meta, guint32 index)
 static void
 mono_backtrace (int limit)
 {
+#ifndef _MSC_VER
 	void *array[limit];
 	char **names;
 	int i;
@@ -5522,6 +5523,7 @@ mono_backtrace (int limit)
 		g_print ("\t%s\n", names [i]);
 	}
 	g_free (names);
+#endif
 }
 #endif
 
